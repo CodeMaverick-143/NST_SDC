c8070b91bb73b53b402adebdc67e360542ccb8a5
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-04-28 01:00:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "NST_SDC_Website",
   description: "Building with purpose, learning by doing.",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
